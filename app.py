5e1ffb6cda69c2e2b0a6a70d77f1437910910d23
Brahathees <brahatheeswaran.parimalam@igreendata.com.au>
2026-08-23 11:42:36 +0530

    Fix app.py security issue and add Snyk Security Scan to pipeline

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def api_counter():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
